0aff11c87f2c0eccbf6dc221fcbc5b1d0bb57b91
君临城下 <3466614871@qq.com>
2020-09-28 14:44:47 +0800

    Site updated: 2020-09-28 14:44:45

---
+++ js/bottomicon.js
@@ -0,0 +1,7 @@
+$('#floating-toc-btn').css("bottom","66px");
+$('#button').attr({
+    "width": "48px !important",
+    "height": "48px !important",
+    "right": "30px",
+    "bottom": "40px !important"
+})
